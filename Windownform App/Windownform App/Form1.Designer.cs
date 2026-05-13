0bb14f08385c0e47c8cdb1a362118ee7db925d88
Quyen Dang <dangthimyquyen1970@gmail.com>
2026-05-13 17:00:27 +0700

    chuc nang dang nhap

--- Windownform App/Windownform App/Form1.Designer.cs
+++ Windownform App/Windownform App/Form1.Designer.cs
@@ -28,13 +28,84 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txt_tendangnhap = new System.Windows.Forms.TextBox();
+            this.txt_matkhau = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btn_dangnhap = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(177, 202);
+            this.label1.Name = "label1";
+            this.label1.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.label1.Size = new System.Drawing.Size(116, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Tên đăng nhập";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // txt_tendangnhap
+            // 
+            this.txt_tendangnhap.Location = new System.Drawing.Point(327, 199);
+            this.txt_tendangnhap.Name = "txt_tendangnhap";
+            this.txt_tendangnhap.Size = new System.Drawing.Size(364, 26);
+            this.txt_tendangnhap.TabIndex = 1;
+            this.txt_tendangnhap.TextChanged += new System.EventHandler(this.txt_tendangnhap_TextChanged);
+            // 
+            // txt_matkhau
+            // 
+            this.txt_matkhau.Location = new System.Drawing.Point(327, 255);
+            this.txt_matkhau.Name = "txt_matkhau";
+            this.txt_matkhau.Size = new System.Drawing.Size(364, 26);
+            this.txt_matkhau.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(177, 258);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(75, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Mật khẩu";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // btn_dangnhap
+            // 
+            this.btn_dangnhap.Location = new System.Drawing.Point(377, 315);
+            this.btn_dangnhap.Name = "btn_dangnhap";
+            this.btn_dangnhap.Size = new System.Drawing.Size(152, 36);
+            this.btn_dangnhap.TabIndex = 4;
+            this.btn_dangnhap.Text = "Đăng nhập";
+            this.btn_dangnhap.UseVisualStyleBackColor = true;
+            this.btn_dangnhap.Click += new System.EventHandler(this.btn_dangnhap_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1033, 548);
+            this.Controls.Add(this.btn_dangnhap);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txt_matkhau);
+            this.Controls.Add(this.txt_tendangnhap);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txt_tendangnhap;
+        private System.Windows.Forms.TextBox txt_matkhau;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btn_dangnhap;
     }
 }
 
--- Windownform App/Windownform App/Form1.cs
+++ Windownform App/Windownform App/Form1.cs
@@ -16,5 +16,42 @@ namespace Windownform_App
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txt_tendangnhap_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void btn_dangnhap_Click(object sender, EventArgs e)
+        {
+            // Lấy dữ liệu người dng nhập vào
+            string username = txt_tendangnhap.Text.Trim();
+            string password = txt_matkhau.Text.Trim();
+
+            // Kiểm tra điều kiện: Tên đăng nhập là email, mật khẩu là MSSV
+            if (username == "dangthimyquyen1970@gmail.com" && password == "0023068")
+            {
+                MessageBox.Show("Đăng nhập thành công!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                MessageBox.Show("Đăng nhập thất bại!", "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
     }
 }
